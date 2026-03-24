3987586587e2911e77282b6a52c0e0f38de39b4d
DZPaspalov24 <DZPaspalov24@codingburgas.bg>
2026-03-24 23:07:30 +0200

    Change the questions

--- TestCraftCode/menu.cpp
+++ TestCraftCode/menu.cpp
@@ -9,14 +9,14 @@ int showMenu() {
 	int choice;
 
 	cout << "-----------------------------------------------------------------------------" << endl;
-	cout << "                           TEST CRAFT-BIOLOGY TESTS                          " << endl;
+	cout << "                           TEST CRAFT-BIOLOGY TESTS" << endl;
 	cout << "-----------------------------------------------------------------------------" << endl;
-	cout << " " << endl;
+	cout << endl;
 	cout << "1. Take a test" << endl;
 	cout << "2. How to use Test Craft" << endl;
 	cout << "3. Test marks" << endl;
 	cout << "4. Exit" << endl;
-	cout << " " << endl;
+	cout << endl;
 	cout << "What page do you want to go to: ";
 	cin >> choice;
 
@@ -30,7 +30,7 @@ void page1() {
 	cout << "-----------------------------------------------------------------------------" << endl;
 	cout << "                                  TEST CRAFT                                 " << endl;
 	cout << "-----------------------------------------------------------------------------" << endl;
-	cout << " " << endl;
+	cout << endl;
 	cout << "Topic 1: Cell test" << endl;
 	cout << "Topic 2: Organisms test" << endl;
 	cout << "Topic 3: Plants test" << endl;
@@ -72,17 +72,17 @@ void page2() {
 	int back;
 
 	cout << "-----------------------------------------------------------------------------" << endl;
-	cout << "                           HOW TO USE-TEST CRAFT                             " << endl;
+	cout << "                           HOW TO USE-TEST CRAFT" << endl;
 	cout << "-----------------------------------------------------------------------------" << endl;
-	cout << " " << endl;
+	cout << endl;
 	cout << "Test Craft is an application designed to help students improve their grades in school or learn something new." << endl;
 	cout << "Within the app, users can take biology tests, complete homework assignments, and prepare for upcoming" << endl;
 	cout << "school exams." << endl;
-	cout << " " << endl;
+	cout << endl;
 	cout << "After completing a test, students enter their name and are placed in a ranking system that includes all" << endl;
 	cout << "students who have taken that particular test. Additionally, users can view their average score across all" << endl;
 	cout << "completed tests and homework assignments, allowing them to track their academic progress over time." << endl;
-	cout << " " << endl;
+	cout << endl;
 	cout << "Press 1 to go back to the main menu: ";
 	cin >> back;
 
